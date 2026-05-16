6ad0232c380ee129c4c3a6ce01afa8d882d6a230
Red <me@nospawnn.com>
2026-05-16 21:25:10 +0100

    remove unneeded `quadletsRoot`

--- flake.nix
+++ flake.nix
@@ -34,14 +34,12 @@
             pkgs = import inputs.nixpkgs { inherit system; };
           }
         );
-
-      quadletsRoot = ../quadlets;
     in
     {
       nixosConfigurations = {
         hv-1 = lib.nixosSystem {
           system = "x86_64-linux";
-          specialArgs = { flake-inputs = inputs; inherit quadletsRoot; };
+          specialArgs.flake-inputs = inputs;
           modules = [
             ./nix/hosts/metal/hv-1
             sops-nix.nixosModules.sops
@@ -50,7 +48,7 @@
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.sharedModules = [ inputs.sops-nix.homeManagerModules.sops ];
-              home-manager.extraSpecialArgs = { inherit inputs quadletsRoot; };
+              home-manager.extraSpecialArgs = { inherit inputs; };
               home-manager.users.services = ./nix/users/services.nix;
             }
           ];
