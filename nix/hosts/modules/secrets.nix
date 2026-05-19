dd1514d337a299b80e55672ad9b24e4432b847e6
Red <me@nospawnn.com>
2026-05-19 12:14:03 +0100

    deploy davis
    DAV server
    TODO: webdac

--- nix/hosts/modules/secrets.nix
+++ nix/hosts/modules/secrets.nix
@@ -5,11 +5,18 @@
     age.keyFile = "/var/lib/sops-nix/keys.txt";
     age.generateKey = true;
 
+    # TODO: this isn't sustainable...
     secrets = {
       immich = {
         sopsFile = ../../../quadlets/immich/secret.env;
         format = "dotenv";
         path = "/home/services/.config/containers/systemd/immich/.env";
+        owner = config.users.users.services.name;
+      };
+      davis = {
+        sopsFile = ../../../quadlets/davis/secret.env;
+        format = "dotenv";
+        path = "/home/services/.config/containers/systemd/davis/.env";
         owner = config.users.users.services.name;
       };
     };
